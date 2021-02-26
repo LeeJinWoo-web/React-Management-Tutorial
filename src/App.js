20005834a5cd31018d5c1ba992888397b0f9da07
LeeJinWoo-web <aoel900202@gmail.com>
2021-02-26 13:48:11 +0900

    component folder생성 및 props 활용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = {
+  'name' : '홍길동',
+  'birthday' : '900202',
+  'gender' : '남자',
+  'jop' : '학생'
+}
 
 class App extends Component {
   render() {
     return(
-      <div className="gray-background">
-         <img src={logo} alt="logo"/>
-        Hello React11111
-      </div>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        jop={customer.jop}
+      />
     );
   }
 }
