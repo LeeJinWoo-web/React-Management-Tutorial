0fb093d98091175bd50494ed8d68fc5773ce781d
LeeJinWoo-web <aoel900202@gmail.com>
2021-02-26 14:14:43 +0900

    고객정보 추가 +배열화 props 순회 전달

--- src/App.js
+++ src/App.js
@@ -2,26 +2,49 @@ import React, {Component} from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/any',
-  'name' : '이진우',
-  'birthday' : '900202',
-  'gender' : '남자',
-  'jop' : '학생'
-}
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/any',
+    'name' : '이진우',
+    'birthday' : '900202',
+    'gender' : '남자',
+    'jop' : '학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '박동희',
+    'birthday' : '891222',
+    'gender' : '남자',
+    'jop' : '학생'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '박시영',
+    'birthday' : '890713',
+    'gender' : '남자',
+    'jop' : '디자이너'
+  }
+]
 
 class App extends Component {
   render() {
     return(
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        jop={customer.jop}
-      />
+      customers.map((arr) => {
+        console.log(arr)
+        return(
+          <Customer
+            id={arr.id}
+            image={arr.image}
+            name={arr.name}
+            birthday={arr.birthday}
+            gender={arr.gender}
+            jop={arr.jop}
+          />
+        )
+      })
     );
   }
 }
