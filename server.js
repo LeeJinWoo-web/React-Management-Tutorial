2b74ef46b5c4a327fe24db8e2ecb81beab83ba21
LeeJinWoo-web <aoel900202@gmail.com>
2021-03-09 21:56:00 +0900

    Create Customer Databae TABLE & Node js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers',(req, res) => {
-    res.send([
-            {
-              'id' : 1,
-              'image' : 'https://placeimg.com/64/64/any',
-              'name' : '이진우',
-              'birthday' : '900202',
-              'gender' : '남자',
-              'jop' : '학생'
-            },
-            {
-              'id' : 2,
-              'image' : 'https://placeimg.com/64/64/2',
-              'name' : '박동희',
-              'birthday' : '891222',
-              'gender' : '남자',
-              'jop' : '학생'
-            },
-            {
-              'id' : 3,
-              'image' : 'https://placeimg.com/64/64/3',
-              'name' : '박시영',
-              'birthday' : '890713',
-              'gender' : '남자',
-              'jop' : '디자이너'
-            }
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+         res.send(rows); 
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
